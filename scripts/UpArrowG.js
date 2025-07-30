64d245d6d7eb2131e22ab216dca4449ad085bcf8
TechByteSo <sstanovojbk@gmail.com>
2025-07-31 00:53:51 +0300

    На всякий

---
+++ scripts/UpArrowG.js
@@ -0,0 +1,33 @@
+/**
+ * Управление кнопкой "Наверх"
+ * 
+ * 1. Показывает/скрывает кнопку при скролле
+ * 2. Обеспечивает плавную прокрутку вверх
+ */
+
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Функция показа/скрытия кнопки
+function toggleScrollToTopButton() {
+  const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  
+  if (scrollPosition > 350) {
+    scrollToTopBtn.style.display = "flex";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+// Инициализация при загрузке
+toggleScrollToTopButton();
+
+// Обработчик скролла
+window.onscroll = toggleScrollToTopButton;
+
+// Обработчик клика
+scrollToTopBtn.addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
